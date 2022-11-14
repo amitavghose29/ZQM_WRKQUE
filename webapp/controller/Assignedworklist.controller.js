0bfb61f0db9aade21256bcca937cbbe2f6ecb94f
Amitav GHOSE <amitav.ghose.external@airbus.com>
2022-11-14 16:09:25 +0000

    Working Queue Changes

--- webapp/controller/Assignedworklist.controller.js
+++ webapp/controller/Assignedworklist.controller.js
@@ -1,16 +1,43 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
+	'sap/m/TablePersoController',
+	'../model/WrkQueuePersoService',
+	'sap/ui/core/mvc/Controller',
 	'sap/ui/core/Fragment'
-], function (Controller, Fragment) {
+], function (TablePersoController, WrkQueuePersoService,Controller, Fragment) {
 	"use strict";
-
+    //var ResetAllMode =  mlibrary.ResetAllMode;
 	return Controller.extend("com.airbus.zqmwrkque.controller.Assignedworklist", {
 		onInit: function () {
+			this._oTPC = new TablePersoController({
+				table: this.byId("wrkQueueTable"),
+				componentName: "wrkQueueApp",
+				//resetAllMode: ServiceReset,
+				persoService: WrkQueuePersoService
+			}).activate();
+
+		},
+		onExit: function () {
+			this._oTPC.destroy();
+		},
+		onCreate: function(oEvent){
+			var oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation"); // get a handle on the global XAppNav service
+			var hash = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
+			target: {
+			semanticObject: "zqmncr",
+			action: "display"
+			}
+			})) || "";
+			oCrossAppNavigator.toExternal({
+			target: {
+			shellHash: hash
+			}
+			});
+
 
 		},
 		onPressCopy:function(){
 			var oView = this.getView();
-			var tab = this.getView().byId("idProductsTable").getSelectedItems();
+			var tab = this.getView().byId("wrkQueueTable").getSelectedItems();
 			if(tab.length === 0){
 				this._pValueHelpDialog = Fragment.load({
 					id: oView.getId(),
@@ -49,5 +76,41 @@ sap.ui.define([
 				oValueHelpDialog.open();
 			});
 		},
+        onPersoButtonPressed: function (oEvent){
+			this._oTPC.openDialog();
+
+		},
+		handleFilterButtonPressed: function(oEvent){
+			if (!this.oFilterFragment) {
+				this.oFilterFragment = sap.ui.xmlfragment("filterFragment", "com.airbus.zqmwrkque.fragments.FilterDialog", this);
+				this.getView().addDependent(this.oFilterFragment);
+			  }
+			  this.oFilterFragment.open();
+
+		},
+		onFilter: function (oEvent) {
+			if (oEvent) {
+				this.columnText = oEvent.getParameters().sortItem.getText();
+				this.columnKey = oEvent.getParameters().sortItem.getKey();
+				this.bDesc = oEvent.getParameters().sortDescending;
+			  }
+			  var oSorter = new sap.ui.model.Sorter(this.columnKey, this.bDesc);
+			  if (this.columnKey === "Ncr" || this.columnKey === "Disc") {
+				
+				if (this.columnKey === "Ncr") {
+				 
+				}
+			  }
+			  var oTable = this.byId("wrkQueueTable");
+			  //this.byId("wrkQueueTable")
+			  oTable.getBinding("items").sort(oSorter);
+			  this.byId("idbuttonfilter").setIcon(this.bDesc ? 'sap-icon://sort-descending' :
+				'sap-icon://sort-ascending');
+
+
+
+
+		}
+
 	});
 });
---
+++ webapp/model/WrkQueuePersoService.js
@@ -0,0 +1,173 @@
+sap.ui.define(['sap/ui/thirdparty/jquery'],
+	function(jQuery) {
+	"use strict";
+
+	// Very simple page-context personalization
+	// persistence service, not for productive use!
+	var WrkQueuePersoService = {
+
+		oData : {
+			_persoSchemaVersion: "1.0",
+			aColumns : [
+				{
+					id: "wrkQueueApp-wrkQueueTable-ncrCol",
+					order: 0,
+					text: "NCR Number",
+					visible: true
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-discCol",
+					order: 1,
+					text: "Disc No",
+					visible: true
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-nctypCol",
+					order: 2,
+					text: "NCR Types",
+					visible: false
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-compCol",
+					order: 3,
+					text: "Component No",
+					visible: false
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-rncCol",
+					order: 4,
+					text: "RNC Priority",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-locCol",
+					order: 5,
+					text: "Location Part",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-detworkCol",
+					order: 6,
+					text: "Det. Work Center",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-detectedCol",
+					order: 7,
+					text: "Detected Date",
+					visible: false
+				}
+			]
+		},
+
+		oResetData : {
+			_persoSchemaVersion: "1.0",
+			aColumns : [
+				{
+					id: "wrkQueueApp-wrkQueueTable-ncrCol",
+					order: 0,
+					text: "NCR Number",
+					visible: true
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-discCol",
+					order: 1,
+					text: "Disc No",
+					visible: false
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-nctypCol",
+					order: 2,
+					text: "NCR Types",
+					visible: false
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-compCol",
+					order: 3,
+					text: "Component No",
+					visible: false
+				},
+				{
+					id: "wrkQueueApp-wrkQueueTable-rncCol",
+					order: 4,
+					text: "RNC Priority",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-locCol",
+					order: 5,
+					text: "Location Part",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-detworkCol",
+					order: 6,
+					text: "Det. Work Center",
+					visible: false
+				},
+                {
+					id: "wrkQueueApp-wrkQueueTable-detectedCol",
+					order: 7,
+					text: "Detected Date",
+					visible: false
+				}
+			]
+		},
+
+
+		getPersData : function () {
+			var oDeferred = new jQuery.Deferred();
+			if (!this._oBundle) {
+				this._oBundle = this.oData;
+			}
+			oDeferred.resolve(this._oBundle);
+			// setTimeout(function() {
+			// 	oDeferred.resolve(this._oBundle);
+			// }.bind(this), 2000);
+			return oDeferred.promise();
+		},
+
+		setPersData : function (oBundle) {
+			var oDeferred = new jQuery.Deferred();
+			this._oBundle = oBundle;
+			oDeferred.resolve();
+			return oDeferred.promise();
+		},
+
+		getResetPersData : function () {
+			var oDeferred = new jQuery.Deferred();
+
+			// oDeferred.resolve(this.oResetData);
+
+			setTimeout(function() {
+				oDeferred.resolve(this.oResetData);
+			}.bind(this), 2000);
+
+			return oDeferred.promise();
+		},
+
+		resetPersData : function () {
+			var oDeferred = new jQuery.Deferred();
+
+			//set personalization
+			this._oBundle = this.oResetData;
+
+			//reset personalization, i.e. display table as defined
+			//this._oBundle = null;
+
+			oDeferred.resolve();
+
+			// setTimeout(function() {
+			// 	this._oBundle = this.oResetData;
+			// 	oDeferred.resolve();
+			// }.bind(this), 2000);
+
+			return oDeferred.promise();
+		},
+
+		
+	};
+
+	return WrkQueuePersoService;
+
+});
